d4cc09d9bedea3b05482b10d6396d7e98ea1d4a5
DarkGamingKni1 <106308109+DarkGamingKni1@users.noreply.github.com>
2025-09-24 22:52:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,6 @@ from discord_webhook import DiscordWebhook, DiscordEmbed
 
 app = FastAPI()
 
-# Root route
-@app.get("/")
-def root():
-    return {"message": "Welcome to the root route!"}
-
-@app.post("/api/sellauth", status_code=status.HTTP_200_OK)
-async def sellauth(request: Request):
-    embed = DiscordEmbed(title="Test", description=f"Headers: {request.headers}\nBody: {request.body()}")
-    send_webhook_message("https://discord.com/api/webhooks/1418947147046588526/Lxs1FBxpDCiDrSyrszabrxtWTmeawWz9IVQuUdfIvuHN-KU2rAK9z9x4pLj2T-4Tsyqg", "", embed)
 
 def send_webhook_message(webhook_url, content, embed):
     webhook = DiscordWebhook(url=webhook_url, content=content)
@@ -22,3 +13,13 @@ def send_webhook_message(webhook_url, content, embed):
         pass
     else:
         print(f"Failed to send message. Status code: {response.status_code}")
+
+# Root route
+@app.get("/")
+def root():
+    return {"message": "Welcome to the root route!"}
+
+@app.post("/api/sellauth", status_code=status.HTTP_200_OK)
+async def sellauth(request: Request):
+    embed = DiscordEmbed(title="Test", description=f"Headers: {request.headers}\nBody: {request.body()}")
+    send_webhook_message("https://discord.com/api/webhooks/1418947147046588526/Lxs1FBxpDCiDrSyrszabrxtWTmeawWz9IVQuUdfIvuHN-KU2rAK9z9x4pLj2T-4Tsyqg", "", embed)
