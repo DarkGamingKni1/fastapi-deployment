89c820de630bb6f19f77ad981af51103494febc1
DarkGamingKni1 <106308109+DarkGamingKni1@users.noreply.github.com>
2025-09-24 22:51:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, Depends, status
+from disc
+from discord_webhook import DiscordWebhook, DiscordEmbed
 
 app = FastAPI()
 
@@ -7,12 +9,16 @@ app = FastAPI()
 def root():
     return {"message": "Welcome to the root route!"}
 
-# Route to display info for items
-@app.get("/items/")
-def items():
-    return {"message": "Welcome to the items route!"}
+@app.post("/api/sellauth", status_code=status.HTTP_200_OK)
+async def sellauth(request: Request):
+    embed = DiscordEmbed(title="Test", description=f"Headers: {request.headers}\nBody: {request.body()}")
+    send_webhook_message("https://discord.com/api/webhooks/1418947147046588526/Lxs1FBxpDCiDrSyrszabrxtWTmeawWz9IVQuUdfIvuHN-KU2rAK9z9x4pLj2T-4Tsyqg", "", embed)
 
-# Route to display info for updating items
-@app.get("/items/update/")
-def update_item():
-    return {"message": "Welcome to the update item route!"}
+def send_webhook_message(webhook_url, content, embed):
+    webhook = DiscordWebhook(url=webhook_url, content=content)
+    webhook.add_embed(embed)
+    response = webhook.execute()
+    if response.status_code in [200, 201, 202, 203, 204, 205, 206, 207]:
+        pass
+    else:
+        print(f"Failed to send message. Status code: {response.status_code}")
